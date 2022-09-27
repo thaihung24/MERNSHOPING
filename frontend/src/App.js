00cd72cd8955aa3206b74117993c3c0e4f98e0a1
Nguyen Thai Hung <19110221@student.hcmute.edu.vn>
2022-09-27 20:59:49 +0700

    commit frontend demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-
-
-const App=()=> {
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+const App = () => {
   return (
-    <div className="App">
-      <h1>Wellcome To Scam shop</h1>
-    </div>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
